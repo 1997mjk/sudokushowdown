9a1f1c03127b935632057936d376cf4bbe848c89
Minjae Kwon <Minjae Kwon>
2016-05-18 14:24:00 -0400

    make it better

--- Public/js/home.js
+++ Public/js/home.js
@@ -1,8 +1,11 @@
-var socket = io();
-socket.emit('join', 'lobby');
-//socket.emit('create', room);
-function createRoom(){
-	var randomRoomName = parseInt(Math.random()*1000000) +"";
-	socket.emit('createRoom', randomRoomName);
-			
-}
+$(function(){
+	var socket = io ();
+	socket.emit('join', 'lobby');
+	//socket.emit('create', room);
+	function createRoom(){
+		alert('hi');
+		var randomRoomName = parseInt(Math.random()*1000000) +"";
+		socket.emit('createRoom', randomRoomName);		
+	}
+
+});
