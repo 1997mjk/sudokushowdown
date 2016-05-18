1f16a5f981bebc1737dc0959aa3ce0ddb311adea
Minjae Kwon <Minjae Kwon>
2016-05-18 18:08:44 -0400

    make it better

--- routes/login.js
+++ routes/login.js
@@ -4,6 +4,7 @@ var flash = require('connect-flash');
 var router = express.Router();
 
 router.get('/login', function(req, res){
+	console.log('user has logged in');
 	res.render('login', { //login.ejs
 		title: 'Log In',
 		isAuthenticated: req.isAuthenticated(),
