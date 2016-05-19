63e93423df47cdc994aa0b13df15d44323f6ed21
Minjae Kwon <Minjae Kwon>
2016-05-18 23:47:47 -0400

    make it better

--- Public/js/home.js
+++ Public/js/home.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
     $("#createRoom").click( function(){
     	// socket.emit('update');
     	// window.location.href='/board';
-    	var roomDiv = "<li><div class='panel panel-primary'><div class='panel-heading'><div class='row'><div class='col-xs-4'><i class='fa fa-users fa-5x'></i></div><div class='col-xs-4 text-center'><div class='huge'>"+ $(boardDimension).val() +"</div></div><div class='col-xs-4 text-right'><div class='huge'>1/"+ $(playerNumber).val() +"</div><div id='creator'>Room: "+name+"</div></div></div></div><a href='#''><div class='panel-footer'><span class='pull-left' id='joinGame'>Join Game!</span><span class='pull-right'><i class='fa fa-arrow-circle-right'></i></span><div class='clearfix'></div></div></a></div></li>";
+    	var roomDiv = "<li><div class='panel panel-primary'><div class='panel-heading'><div class='row'><div class='col-xs-4'><i class='fa fa-users fa-5x'></i></div><div class='col-xs-4 text-center'><div class='huge'>"+ $(boardDimension).val() +"</div></div><div class='col-xs-4 text-right'><div class='huge'>1/"+ $(playerNumber).val() +"</div><div id='creator'>Room: "+name+"</div></div></div></div><a href='/board'><div class='panel-footer'><span class='pull-left' id='joinGame'>Join Game!</span><span class='pull-right'><i class='fa fa-arrow-circle-right'></i></span><div class='clearfix'></div></div></a></div></li>";
     	$(roomDiv).hide().appendTo("#activeRoomsTimeline").fadeIn(1000);
     });
 
@@ -24,7 +24,7 @@ $(document).ready(function () {
 
     socket.on('roomList', function(name){
     	console.log('updating rooms');
-    	var roomDiv = "<li><div class='panel panel-primary'><div class='panel-heading'><div class='row'><div class='col-xs-4'><i class='fa fa-users fa-5x'></i></div><div class='col-xs-4 text-center'><div class='huge'>"+ $(boardDimension).val() +"</div></div><div class='col-xs-4 text-right'><div class='huge'>1/"+ $(playerNumber).val() +"</div><div id='creator'>Room: "+name+"</div></div></div></div><a href='#''><div class='panel-footer'><span class='pull-left' id='joinGame'>Join Game!</span><span class='pull-right'><i class='fa fa-arrow-circle-right'></i></span><div class='clearfix'></div></div></a></div></li>";
+    	var roomDiv = "<li><div class='panel panel-primary'><div class='panel-heading'><div class='row'><div class='col-xs-4'><i class='fa fa-users fa-5x'></i></div><div class='col-xs-4 text-center'><div class='huge'>"+ $(boardDimension).val() +"</div></div><div class='col-xs-4 text-right'><div class='huge'>1/"+ $(playerNumber).val() +"</div><div id='creator'>Room: "+name+"</div></div></div></div><a href='/board'><div class='panel-footer'><span class='pull-left' id='joinGame'>Join Game!</span><span class='pull-right'><i class='fa fa-arrow-circle-right'></i></span><div class='clearfix'></div></div></a></div></li>";
     	// var roomDiv = "<div>MumeiLover404</div>";
     	$(roomDiv).hide().appendTo("#activeRoomsTimeline").fadeIn(1000);
 	});
